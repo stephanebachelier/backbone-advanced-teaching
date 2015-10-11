953749be28d3a3c1ff4a3ef4ef3d02d58f031a66
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-10-11 21:43:25 +0200

    feat(router): add a Backbone router instance

--- 01-setup/app/scripts/main.js
+++ 01-setup/app/scripts/main.js
@@ -2,10 +2,35 @@
 
 console.log('Hello world!');
 
+var Router = Backbone.Router.extend({
+  routes: {
+    inbox: 'inbox',
+    compose: 'compose',
+    'inbox/:id': 'message'
+  },
+
+  inbox: function () {
+    debugger;
+  },
+
+  compose: function () {
+    debugger;
+  },
+
+  message: function (mid) {
+    debugger;
+  }
+});
+
+var router = new Router();
+
 var app = new Mn.Application();
 
 app.on('start', function () {
   console.log('Marionette application started');
+
+  // router will intercept route change only when history will have been started
+  Backbone.history.start();
 });
 
 app.start();
