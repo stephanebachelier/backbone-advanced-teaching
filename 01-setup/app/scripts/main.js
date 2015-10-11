83034b7b7905a18fdd7656fa5d8767683d1b0d24
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-10-11 21:37:13 +0200

    feat(application): instanciate basic marionette application

--- 01-setup/app/scripts/main.js
+++ 01-setup/app/scripts/main.js
@@ -1,1 +1,11 @@
+'use strict';
+
 console.log('Hello world!');
+
+var app = new Mn.Application();
+
+app.on('start', function () {
+  console.log('Marionette application started');
+});
+
+app.start();
