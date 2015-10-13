bef446fec9651a45ea670de42fe71cefb246cb66
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-10-13 17:36:10 +0200

    feat(inbox): inject search form

--- 02-modules/app/scripts/views/inbox/layout.js
+++ 02-modules/app/scripts/views/inbox/layout.js
@@ -1,16 +1,17 @@
 define([
   'marionette',
   'templates',
-  './message-list'
+  './message-list',
+  './form'
 ],
-function (Marionette, templates, MessageListView) {
+function (Marionette, templates, MessageListView, FormView) {
   'use strict';
 
   return Marionette.LayoutView.extend({
     template: templates['inbox/layout'],
 
     regions: {
-      form: 'form',
+      form: '[data-widget=form]',
       list: '[data-widget=list]'
     },
 
@@ -18,6 +19,8 @@ function (Marionette, templates, MessageListView) {
       this.list.show(new MessageListView({
         collection: this.getOption('collection')
       }));
+
+      this.form.show(new FormView());
     }
   });
 });
