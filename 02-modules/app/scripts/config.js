650d04a4b07c74984a0699e31de45b1944414d51
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-10-13 11:23:44 +0200

    chore(require): setup json plugin

--- 02-modules/app/scripts/config.js
+++ 02-modules/app/scripts/config.js
@@ -1,11 +1,15 @@
 require.config({
   paths: {
+    '#config': '../config',
+
     jquery: '../bower_components/jquery/dist/jquery',
     underscore: '../bower_components/underscore/underscore',
     backbone: '../bower_components/backbone/backbone',
     'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
     'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
     marionette: '../bower_components/backbone.marionette/lib/core/backbone.marionette',
+    text: '../bower_components/text/text',
+    json: '../bower_components/requirejs-plugins/src/json'
   },
 
   deps: ['jquery', 'underscore', 'backbone', 'marionette']
