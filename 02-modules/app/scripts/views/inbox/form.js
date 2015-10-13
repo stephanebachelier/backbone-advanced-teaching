ea1a5f5b9927469913a579af41f03b99d36001df
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-10-13 17:43:03 +0200

    feat(search): retrieve user input

--- 02-modules/app/scripts/views/inbox/form.js
+++ 02-modules/app/scripts/views/inbox/form.js
@@ -7,6 +7,20 @@ function (Marionette, templates) {
 
   return Marionette.ItemView.extend({
     tagName: 'form',
-    template: templates['inbox/form']
+    template: templates['inbox/form'],
+
+    events: {
+      submit: 'onFormSubmited'
+    },
+
+    onFormSubmited: function (e) {
+      e.preventDefault(); // stop page from refreshing
+      e.stopPropagation(); // stop from bubbling; -> phase 3
+
+      var input = this.$('[name=search]').val();
+
+      // special dedicace pour IE ~8
+      return false;
+    }
   });
 });
