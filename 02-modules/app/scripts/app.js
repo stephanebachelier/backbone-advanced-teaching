c2a8d9e86fa78bedbb21fe9e88bef95a2e637118
Stéphane Bachelier <stephane.bachelier@gmail.com>
2015-10-13 10:45:20 +0200

    feat(messages): instanciate message collection

--- 02-modules/app/scripts/app.js
+++ 02-modules/app/scripts/app.js
@@ -1,13 +1,18 @@
 define([
-  'marionette'
+  'marionette',
+  'models/messages'
 ],
-function (Marionette) {
+function (Marionette, Messages) {
   'use strict';
 
   var app = new Marionette.Application();
 
   app.on('start', function () {
     console.log('start');
+
+    // messages -> var globale
+    var messageList = new Messages(messages);
+    console.log(messageList.length);
   });
 
   return app;
